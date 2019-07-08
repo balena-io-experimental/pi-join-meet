51211c7bafc37c5d313d728847c523e804af09a3
Marios Balamatsias <mbalamatsias@gmail.com>
2019-07-08 18:04:35 +0300

    Join Google Meet
    Signed-off-by: Marios Balamatsias <mbalamatsias@gmail.com>

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer-extra');
+puppeteer.use(require('puppeteer-extra-plugin-stealth')());
+
+var path = require('path');
+var express = require('express');
+var app = express();
+var dir = path.join(__dirname, 'public');
+app.use(express.static(dir));
+app.listen(3000, function () {
+	console.log('Listening on http://localhost:3000/');
+});
 
 async function sleep(ms) {
 	return await new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function getConfig() {
+	return {
+		executablePath: '/usr/bin/chromium-browser',
+		args: [
+			'--no-sandbox',
+			'--use-fake-ui-for-media-stream',
+		]
+	}
+}
+
 (async () => {
 	try {
-		const browser = await puppeteer.launch({
-			executablePath: '/usr/bin/chromium-browser',
-			args: ['--no-sandbox', '--use-fake-ui-for-media-stream']
-		});
+		const config = await getConfig();
+		const browser = await puppeteer.launch(config);
 		const page = await browser.newPage();
-		const URL = process.env.MEET_URL || `https://meet.jit.si/${Math.random().toString(36).substring(2)}`;
+
+		const URL = 'https://meet.google.com/rfn-pdbt-wvj';
 		console.log(`Joined: ${URL}`);
 		await page.goto(URL);
-		/* Ensuring Cam and Mic are always On */
-		const camSelector = '#new-toolbox > div.toolbox-content > div.button-group-center > div:nth-child(3) > div > div > i';
-		const micSelector = '#new-toolbox > div.toolbox-content > div.button-group-center > div:nth-child(1) > div > div > i';
-		await page.waitForSelector(camSelector);
-		await page.waitForSelector(micSelector);
+
+		//await page.waitForNavigation();
 		while (true) {
-			var camClassName = await page.evaluate((s) => {
-				return document.querySelector(s).className;
-			}, camSelector);
-			var micClassName = await page.evaluate((s) => {
-				return document.querySelector(s).className;
-			}, micSelector);
-			if (camClassName === 'icon-camera-disabled toggled') {
-				await page.click(camSelector);
-			}
-			if (micClassName === 'icon-mic-disabled toggled') {
-				await page.click(micSelector);
-			}
-			await sleep(5000);
+			await page.screenshot({ path: "./public/test.png" });
+			await sleep(2000);
 		}
 	} catch (err) {
 		console.error(err);
